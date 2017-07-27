c34e1b87a1caecc90db952eb118eeafda997706a
Matěj Sychra <suculent@me.com>
2017-07-27 13:20:45 +0200

    updated docs

--- thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
+++ thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
@@ -2,31 +2,25 @@
 
 #include "Arduino.h"
 
-#include "Thinx.h"
+#include "./thinx-lib-esp8266-arduinoc/src/thinx.h"
 #include "./thinx-lib-esp8266-arduinoc/src/thinx-lib-esp.h"
-
-#define __DEBUG_WIFI__ /* use as fallback when device gets stucked with incorrect WiFi configuration, overwrites Flash in ESP */
 
 THiNX* thx = NULL;
 
 void setup() {
+  
   Serial.begin(115200);
+  
+#ifdef DEBUG
   while (!Serial);
-
-  Serial.printf("Sketch size: %u\n", ESP.getSketchSize());
-  Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
   Serial.setDebugOutput(true);
-
-#ifdef __DEBUG_WIFI__
-  WiFi.begin("HAVANA", "1234567890");
 #endif
 
-  thx = new THiNX(thinx_api_key); // why do we have to call it all over? MQTT callback should be optinally set from here...
-  Serial.println("Setup completed.");
+  thx = new THiNX(thinx_api_key);
 }
 
 void loop()
 {
   delay(10000);  
-  thx->loop(); // check MQTT status, reconnect, etc.
+  thx->loop(); // this is crucial, call loop() from time to time to receive MQTT callback
 }
