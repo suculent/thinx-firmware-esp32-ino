0b52c4d402cf02971efe4f57b7a1b22d93429e40
Matěj Sychra <suculent@me.com>
2017-08-08 23:29:27 +0200

    disabled incomplete stuff for testing builder

--- thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
+++ thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
@@ -1,26 +1,36 @@
 /* OTA enabled firmware for Wemos D1 (ESP 8266, Arduino) */
 
+// Warning! Currently does not work in Arduino IDE, only in PlatformIO.
+
 #include "Arduino.h"
 
-#include "./thinx-lib-esp8266-arduinoc/src/thinx.h"
-#include "./thinx-lib-esp8266-arduinoc/src/thinx-lib-esp.h"
+#include "thinx-lib-esp8266-arduinoc/src/thinx-lib-esp.h"
 
-THiNX* thx = NULL;
-
-void setup() {
-  
-  Serial.begin(115200);
-  
-#ifdef DEBUG
-  while (!Serial);
-  Serial.setDebugOutput(true);
+#define __DEBUG_WIFI__ /* use as fallback when device gets stucked with incorrect WiFi configuration, overwrites Flash in ESP */
+#ifdef __DEBUG_WIFI__
+#include "Settings.h" /* inject WiFi SSID/password here for testing */
 #endif
 
-  thx = new THiNX(THINX_API_KEY); // should not be used when empty string with fallback to captive portal
+//THiNX thx;
+
+void setup() {
+  Serial.begin(115200);
+
+#ifdef __DEBUG__
+  while (!Serial);
+#else
+  delay(500);
+#endif
+
+  const char* apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732";
+  //thx = THiNX(apikey);
+  
+  Serial.println("Setup completed.");
 }
 
 void loop()
 {
-  delay(10000);  
-  thx->loop(); // this is crucial, call loop() from time to time to receive MQTT callback
+  delay(10000);
+  //thx.loop(); // registers, checks MQTT status, reconnects, updates, etc.
+  Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
 }
