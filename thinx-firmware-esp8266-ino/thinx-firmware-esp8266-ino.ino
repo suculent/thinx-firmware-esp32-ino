c796c171dc598c11a1ea3b29a6a3def9b7b2949d
Matěj Sychra <suculent@me.com>
2017-08-09 15:51:41 +0200

    updated to valid and buildable Arduino library

--- thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
+++ thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
@@ -1,17 +1,14 @@
 /* OTA enabled firmware for Wemos D1 (ESP 8266, Arduino) */
 
-// Warning! Currently does not work in Arduino IDE, only in PlatformIO.
-
 #include "Arduino.h"
-
-#include "thinx-lib-esp8266-arduinoc/src/thinx-lib-esp.h"
+#include <THiNXLib.h>
 
 #define __DEBUG_WIFI__ /* use as fallback when device gets stucked with incorrect WiFi configuration, overwrites Flash in ESP */
 #ifdef __DEBUG_WIFI__
 #include "Settings.h" /* inject WiFi SSID/password here for testing */
 #endif
 
-//THiNX thx;
+THiNX thx;
 
 void setup() {
   Serial.begin(115200);
@@ -23,7 +20,7 @@ void setup() {
 #endif
 
   const char* apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732";
-  //thx = THiNX(apikey);
+  thx = THiNX(apikey);
   
   Serial.println("Setup completed.");
 }
@@ -31,6 +28,6 @@ void setup() {
 void loop()
 {
   delay(10000);
-  //thx.loop(); // registers, checks MQTT status, reconnects, updates, etc.
+  thx.loop(); // registers, checks MQTT status, reconnects, updates, etc.
   Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
 }
