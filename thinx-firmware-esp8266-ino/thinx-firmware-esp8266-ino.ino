1ec87b3caf3653524f71db22a298be9454dd40d6
Matěj Sychra <suculent@me.com>
2017-10-27 20:56:49 +0200

    updated example, use with Arduino Library Manager (library THiNXLib 0.2.98 and newer)

--- thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
+++ thinx-firmware-esp8266-ino/thinx-firmware-esp8266-ino.ino
@@ -1,26 +1,34 @@
-/* OTA enabled firmware for Wemos D1 (ESP 8266, Arduino) */
+/* THiNX firmware for ESP 8266 (Arduino) */
 
+#include <dummy.h>
 #include "Arduino.h"
+
+#define __DEBUG__ // wait for serial port on boot
+
+// 1. Include the THiNXLib
 #include <THiNXLib.h>
 
+// 2. Declare
 THiNX thx;
-const char* apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732"; // required to checkin
 
 void setup() {
   Serial.begin(115200);
+
 #ifdef __DEBUG__
   while (!Serial);
-#else
-  delay(500);
-#endif  
-  Serial.println("Setup started.");
-  thx = THiNX(apikey);  
-  Serial.println("Setup completed.");
+  delay(5000);
+  Serial.setDebugOutput(true);
+#endif
+
+  // 4. Initialize
+  Serial.println("\n");
+  Serial.println("*TH: Initializing in 5 seconds...");
+  delay(5000);
+  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732"); // 4. initialize with API Key
 }
 
 void loop()
 {
-  delay(10000);
-  thx.loop(); // registers, checks MQTT status, reconnects, updates, etc.
-  Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
+  // 4. Wait for WiFI, register, check MQTT, reconnect, update, process changes...
+  thx.loop();
 }
