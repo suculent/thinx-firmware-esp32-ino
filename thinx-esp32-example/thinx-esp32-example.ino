096e29b3d1b777e950c447708c0a009ab38e5472
Matěj Sychra <suculent@me.com>
2018-04-13 10:54:55 +0200

    rebuilding example repo

--- thinx-esp32-example/thinx-esp32-example.ino
+++ thinx-esp32-example/thinx-esp32-example.ino
@@ -2,19 +2,23 @@
 #include <THiNX32Lib.h>
 
 #define ARDUINO_IDE
-#define __DEBUG__
+
 
 // Import SSID, PASSWORD and API KEY from file not included in repository
+#ifndef THINXCI
+#import "settings.h"
+#else
 // Those are just dummy demo values. Never store those in public repository!
 // Save them to settings.h and use .gitigore to keep file on your machine only.
 // THiNX CI will be able to add the apikey and owner_id on its own. You can configure
 // optinally THINX_ENV_SSID and THINX_ENV_PASS Environment Variables to inject those from CI.
 
 // Add this to your settings.h and ignore this file from repository before committing!
-const char *apikey = "8e8af4640fee5d8c0649c7b08bcecc3cd379cc777325ccaacc73a11372d5ed6a";
-const char *owner_id = "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12";
+const char *apikey = "";
+const char *owner_id = "";
 const char *ssid = "THiNX-IoT+";
 const char *pass = "<enter-your-ssid-password>";
+#endif
 
 THiNX thx;
 
@@ -33,7 +37,7 @@ void setup() {
   WiFi.begin(ssid, pass);
 #endif
 
-  thx = THiNX(apikey, owner_id);
+  thx = THiNX(apikey);
 
   // Enter API Key and Owner ID (should be faster)
   //thx = THiNX(apikey, owner_id);
